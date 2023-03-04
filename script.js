b386d5f91115288811a512b77940da60352fd6a7
Assel Al-Assel <aseel1234500000@gmail.com>
2023-03-04 09:55:08 +0200

    create css file

---
+++ script.js
@@ -0,0 +1,33 @@
+const product = [
+    {
+        name: "iphone",
+        price: "1000",
+        image: "assets/images/iphone.jpg",
+        addedToCart: false
+    }, {
+        name: "samsung",
+        price: "1000",
+        image: "assets/images/samsung.jpg",
+        addedToCart: false
+    }, {
+        name: "nokia",
+        price: "1000",
+        image: "assets/images/nokia.jpg",
+        addedToCart: false
+    }, {
+        name: "sony",
+        price: "1000",
+        image: "assets/images/sony.jpg",
+        addedToCart: false
+    }, {
+        name: "oppo",
+        price: "1000",
+        image: "assets/images/oppo.jpg",
+        addedToCart: false
+    }, {
+        name: "vivo",
+        price: "1000",
+        image: "assets/images/vivo.jpg",
+        addedToCart: false
+    }
+]

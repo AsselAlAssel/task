dc853f7f95a1080357c00b3998972798174bcbf0
Assel Al-Assel <aseel1234500000@gmail.com>
2023-03-04 17:17:55 +0200

    handle add to cart btn

--- script.js
+++ script.js
@@ -1,33 +1,79 @@
-const product = [
-    {
-        name: "iphone",
-        price: "1000",
-        image: "assets/images/iphone.jpg",
-        addedToCart: false
-    }, {
-        name: "samsung",
-        price: "1000",
-        image: "assets/images/samsung.jpg",
-        addedToCart: false
-    }, {
-        name: "nokia",
-        price: "1000",
-        image: "assets/images/nokia.jpg",
-        addedToCart: false
-    }, {
-        name: "sony",
-        price: "1000",
-        image: "assets/images/sony.jpg",
-        addedToCart: false
-    }, {
-        name: "oppo",
-        price: "1000",
-        image: "assets/images/oppo.jpg",
-        addedToCart: false
-    }, {
-        name: "vivo",
-        price: "1000",
-        image: "assets/images/vivo.jpg",
-        addedToCart: false
-    }
-]
+import { products } from './data.js';
+
+const domElements = {
+    productsContainer: document.querySelector('.products-container'),
+    cartBtn: document.querySelector('.cart-btn'),
+    viewModal: document.querySelector('.view-modal'),
+};
+
+const addElementsToDomElements = (domElements) => {
+    domElements['addedToCartButtons'] = document.querySelectorAll('.btn--add');
+    domElements['viewProductButtons'] = document.querySelectorAll('.btn--view');
+}
+
+const renderProducts = (products, domElements) => {
+    let productsHtml = '';
+    products.forEach(product => {
+        productsHtml += `
+            <div class="product-card" data-id=${product.id}>
+            <div class="product--img">
+                <img src="${product.image}" alt="${product.name}">
+            </div>
+           <div class="product--box">
+           <div class="product--info flex">
+           <h3>${product.name}</h3>
+           <p>$${product.price}</p>
+        </div>
+        <div class="product--button flex">
+            <button class="btn btn--add">Add to cart</button>
+            <button class="btn btn--view">View</button>
+        </div>
+        </div>
+            </div>  
+        `;
+    });
+    domElements.productsContainer.innerHTML = productsHtml;
+
+    addElementsToDomElements(domElements);
+}
+renderProducts(products, domElements);
+
+const handleAddToCart = (btnElement, products, productId) => {
+    btnElement.classList.add('active');
+    btnElement.innerText = "Remove from cart"
+    products[productId].addedToCart = true
+}
+const handleRemoveFromCart = (btnElement, products, productId) => {
+    btnElement.classList.remove('active');
+    btnElement.innerText = "Add to cart"
+    products[productId].addedToCart = false
+}
+
+const addEventListenerForAddToCartBtns = (addedToCartButtons, products) => {
+    addedToCartButtons.forEach(btn => {
+        btn.addEventListener('click', () => {
+            const productCard = btn.closest('.product-card');
+            const productId = productCard.dataset.id;
+            if (btn.classList.contains('active')) {
+                handleRemoveFromCart(btn, products, productId)
+            } else {
+                handleAddToCart(btn, products, productId)
+            }
+        });
+    });
+}
+addEventListenerForAddToCartBtns(domElements.addedToCartButtons, products);
+
+const addEventListenerForViewProductBtns = (viewProductButtons, viewModal, products) => {
+    viewProductButtons.forEach(btn => {
+        btn.addEventListener('click', () => {
+            const productCard = btn.closest('.product-card');
+            const productId = productCard.dataset.id;
+
+            renderProductModal(products[productId], viewModal);
+        });
+    });
+}
+
+addEventListenerForViewProductBtns(domElements.viewProductButtons, domElements.viewModal, products);
+

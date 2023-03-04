89a68b0973154cc1e73da57f2efff94da62388a0
Assel Al-Assel <aseel1234500000@gmail.com>
2023-03-04 17:16:37 +0200

    change the id in products

--- data.js
+++ data.js
@@ -1,39 +1,39 @@
 export const products = [
     {
-        id: 1,
+        id: 0,
         name: 'Iphone 12',
         price: 1000,
-        image: 'assets/images/iphone-12.jpg',
+        image: 'assets/images/iphone-12.png',
         addedToCart: false,
     }, {
-        id: 2,
+        id: 1,
         name: 'Iphone 11',
         price: 900,
-        image: 'assets/images/iphone-11.jpg',
+        image: 'assets/images/iphone-11.png',
+        addedToCart: false
+    }, {
+        id: 2,
+        name: 'Iphone 13',
+        price: 800,
+        image: 'assets/images/iphone-13.png',
         addedToCart: false
     }, {
         id: 3,
-        name: 'Iphone 13',
-        price: 800,
-        image: 'assets/images/iphone-13.jpg',
+        name: 'Iphone 5',
+        price: 700,
+        image: 'assets/images/iphone-5.png',
         addedToCart: false
     }, {
         id: 4,
-        name: 'Iphone 5',
-        price: 700,
-        image: 'assets/images/iphone-5.jpg',
+        name: 'Samsung 6',
+        price: 600,
+        image: 'assets/images/samsung-6.png',
         addedToCart: false
     }, {
         id: 5,
-        name: 'Samsung 6',
-        price: 600,
-        image: 'assets/images/samsung-6.jpg',
-        addedToCart: false
-    }, {
-        id: 6,
         name: 'Samsung 4',
         price: 500,
-        image: 'assets/images/samsung-4.jpg',
+        image: 'assets/images/samsung-4.png',
         addedToCart: false
     }
 ]

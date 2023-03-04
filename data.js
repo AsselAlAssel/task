8d2e26016f452262c7a7b66340e846f3e19e2884
Assel Al-Assel <aseel1234500000@gmail.com>
2023-03-04 10:55:24 +0200

    define the data to use it in project

---
+++ data.js
@@ -0,0 +1,39 @@
+export const products = [
+    {
+        id: 1,
+        name: 'Iphone 12',
+        price: 1000,
+        image: 'assets/images/iphone-12.jpg',
+        addedToCart: false,
+    }, {
+        id: 2,
+        name: 'Iphone 11',
+        price: 900,
+        image: 'assets/images/iphone-11.jpg',
+        addedToCart: false
+    }, {
+        id: 3,
+        name: 'Iphone 13',
+        price: 800,
+        image: 'assets/images/iphone-13.jpg',
+        addedToCart: false
+    }, {
+        id: 4,
+        name: 'Iphone 5',
+        price: 700,
+        image: 'assets/images/iphone-5.jpg',
+        addedToCart: false
+    }, {
+        id: 5,
+        name: 'Samsung 6',
+        price: 600,
+        image: 'assets/images/samsung-6.jpg',
+        addedToCart: false
+    }, {
+        id: 6,
+        name: 'Samsung 4',
+        price: 500,
+        image: 'assets/images/samsung-4.jpg',
+        addedToCart: false
+    }
+]
